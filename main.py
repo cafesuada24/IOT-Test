c537c527c3b1c5d097b98e97ef0f68f855d1c4c9
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-05-20 08:19:40 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from lib import AMSpi
+
+def main() -> None:
+    with AMSpi() as amspi:
+        amspi.set_74HC595_pins(21, 20, 16)
+        amspi.set_L293D_pins(5, 6, 13, 19)
+
+        amspi.run_dc_motors([amspi.DC_Motor_1,amspi.DC_Motor_2, amspi.DC_Motor_3, amspi.DC_Motor_4])
+
+if __name__ == '__main__':
+    main()
+
