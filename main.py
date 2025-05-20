afdacd2f82708341fb753d74d1beec2102f63d59
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-05-20 08:42:27 +0700

    test

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main() -> None:
         amspi.set_74HC595_pins(21, 20, 16)
         amspi.set_L293D_pins(5, 6, 13, 19)
 
-        amspi.run_dc_motors([amspi.DC_Motor_1,amspi.DC_Motor_2, amspi.DC_Motor_3, amspi.DC_Motor_4])
+        amspi.run_dc_motors([amspi.DC_Motor_1])
         
         sleep(3)
 
-        amspi.stop_dc_motors([amspi.DC_Motor_1,amspi.DC_Motor_2, amspi.DC_Motor_3, amspi.DC_Motor_4])
+        amspi.stop_dc_motors([amspi.DC_Motor_1])
 
 if __name__ == '__main__':
     main()
